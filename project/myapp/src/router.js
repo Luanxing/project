d0164531a249104395b06dd9021fa3b5d1a583f7
郭贺吉 <guoheji@liliwangxiang.mshome.net>
2019-06-14 10:34:44 +0800

    合并郑帅

---
+++ project/myapp/src/router.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '@/views/Main/Main.vue'
+import Find from '@/views/Find/Find.vue'
+import Shopping from '@/views/Shopping/Shopping.vue'
+import Tickets from '@/views/Tickets/Tickets.vue'
+import Filmreview from '@/views/Find/finds/filmreview'
+import Leaderboard from '@/views/Find/finds/leaderboard'
+import News from '@/views/Find/finds/news'
+import Trailer from '@/views/Find/finds/trailer'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/main',
+      component: Main
+    },
+    {
+      path: '/tickets',
+      component: Tickets
+    },
+    {
+      path: '/find',
+      component: Find,
+      children: [
+        {
+          path: 'filmreview',
+          component:Filmreview
+        },
+        {
+          path: 'leaderboard',
+          component: Leaderboard
+        },
+        {
+          path: 'news',
+          component: News
+        },
+        {
+          path: 'trailer',
+          component: Trailer
+        },
+        {
+          path: '',
+          redirect: '/find/news'
+        }
+      ]
+    },
+    {
+      path: '/shopping',
+      component: Shopping
+    },
+    {
+      path: '/',
+      component: Main
+      
+    }
+  ]
+})
---
+++ project/myapp/src/views/Find/Find.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <findheader></findheader>
+  <router-view></router-view>
+  </div>
+</template>
+<script>
+import findheader from './finds/findheader'
+export default {
+	
+	components: {
+		findheader
+	}
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
